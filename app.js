159969796331bf7f2a0c543e0e2150b95c54cd36
Hiren Khatri <63735062+hk-0@users.noreply.github.com>
2021-09-23 12:32:46 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Working on CI/CD AWS tutorial!'));
+app.get('/', (req, res) => res.send('Testing if Full CI/CD pipeline is working'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
